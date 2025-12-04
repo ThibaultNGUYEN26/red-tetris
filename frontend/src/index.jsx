6cc58a2b1bc76ed8224e4ff967d0cc16c6798f56
ThibaultNGUYEN26 <thibault.nguyen26@gmail.com>
2025-12-04 11:42:20 +0100

    feat(components): add GoodClouds and UsernameMenu components with styles
    - Introduced GoodClouds component for animated cloud backgrounds.
    - Added UsernameMenu component for user input with validation.
    - Created corresponding CSS files for styling both components.
    - Updated index files to include new components in the application layout.

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,12 +1,18 @@
 import './index.css'
-import GoodClouds from './components/GoodClouds.jsx'
+import GoodClouds from './components/GoodClouds/GoodClouds.jsx'
+import UsernameMenu from './components/UsernameMenu/UsernameMenu.jsx'
 
 function Index() {
 
   return (
-    <div className='sky-background'>
-      <GoodClouds />
-    </div>
+    <>
+      <div className='sky-background'>
+        <GoodClouds />
+      </div>
+      <div className='content-wrapper'>
+        <UsernameMenu />
+      </div>
+    </>
   )
 }
 
