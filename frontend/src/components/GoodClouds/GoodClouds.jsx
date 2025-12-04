4780def6864072fe4b06a8e1df9e02a3a10575e3
ThibaultNGUYEN26 <thibault.nguyen26@gmail.com>
2025-12-04 16:01:19 +0100

    feat(components): add an additional cloud div to GoodClouds component

--- frontend/src/components/GoodClouds/GoodClouds.jsx
+++ frontend/src/components/GoodClouds/GoodClouds.jsx
@@ -6,6 +6,7 @@ function GoodClouds() {
       <div className="cloud"></div>
       <div className="cloud"></div>
       <div className="cloud"></div>
+      <div className="cloud"></div>
     </>
   )
 }
